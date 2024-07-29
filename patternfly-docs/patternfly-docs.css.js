aeef0b503e8d73a52eb4a5c871d8b361132cf911
Matt Nolting <matthew.nolting@gmail.com>
2024-07-29 08:45:07 -0400

    update

--- patternfly-docs/patternfly-docs.css.js
+++ patternfly-docs/patternfly-docs.css.js
@@ -2,7 +2,7 @@
 import '../dist/patternfly.css';
 
 // Utilities
-import '../dist/patternfly-addons.css';
+// import '../dist/patternfly-addons.css';
 
 // Global theme CSS
-import '@patternfly/documentation-framework/global.css';
+// import '@patternfly/documentation-framework/global.css';
